52e9fe9ec8b68a8bdbd2e744d30d714d604a34f2
Xavier Er <simpleton_er@hotmail.com>
2014-10-30 17:11:06 +0800

    Refine main.go

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"github.com/XavierEr/UedBetMite/Model"
 	"github.com/XavierEr/UedBetMite/UedBetDataJson"
 	"io/ioutil"
 	"net/http"
@@ -13,40 +14,38 @@ var baseUri = "http://sb.uedbet.com/zh-cn/OddsService/"
 func main() {
 	unixUtcDateTimeNowMillisecond := getUnixUtcDateTimeNowMillisecond()
 	queryOddsParam := queryOddsParam{utcDateTime: unixUtcDateTimeNowMillisecond, sportId: 1, programmeId: 0, pageType: 1, uiBetType: "am", displayView: 2, pageNo: 0, oddsType: 2, sortBy: 1, isFirstLoad: true, MoreBetEvent: "null"}
-	getOddsUrl := getOddsUri(queryOddsParam)
-	fmt.Println(getOddsUrl)
+	oddsUrl := getOddsUrl(queryOddsParam)
 
-	resp, err := http.Get(getOddsUrl)
+	uedBetData := getUedBetData(oddsUrl)
+	fmt.Println(uedBetData.TotalPages)
+	//~ Save odds data to mongo db here
+
+	for i := 1; i < uedBetData.TotalPages; i++ {
+		queryOddsParam.pageNo = i
+		oddsUrl = getOddsUrl(queryOddsParam)
+		uedBetData := getUedBetData(oddsUrl)
+		fmt.Println(uedBetData.TotalPages)
+		//~ Save odds data to mongo db here
+	}
+}
+
+func getUedBetData(oddsUrl string) model.UedBetData {
+	resp, err := http.Get(oddsUrl)
 	if err != nil {
 		fmt.Println(err)
 	}
 	defer resp.Body.Close()
 
-	if resp.StatusCode == 200 {
-		body, err := ioutil.ReadAll(resp.Body)
-		if err != nil {
-			fmt.Println(err)
-		}
-
-		uedBetData := uedBetDataJson.Parse(body)
-		fmt.Println(uedBetData.TotalPages)
-
-		for i := 1; i < uedBetData.TotalPages; i++ {
-			queryOddsParam.pageNo = i
-			getOddsUrl = getOddsUri(queryOddsParam)
-			fmt.Println(getOddsUrl)
-		}
+	//~ if resp.StatusCode == 200 {
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println(err)
 	}
+	return uedBetDataJson.Parse(body)
+	//~ }
 }
 
-type queryOddsParam struct {
-	utcDateTime                                                           int64
-	sportId, programmeId, pageType, displayView, pageNo, oddsType, sortBy int
-	uiBetType, MoreBetEvent                                               string
-	isFirstLoad                                                           bool
-}
-
-func getOddsUri(queryOddsParam queryOddsParam) string {
+func getOddsUrl(queryOddsParam queryOddsParam) string {
 	return fmt.Sprintf("%vGetOdds?_=%v&sportId=%v&programmeId=%v&pageType=%v&uiBetType=%v&displayView=%v&pageNo=%v&oddsType=%v&sortBy=%v&isFirstLoad=%t&MoreBetEvent=%v",
 		baseUri,
 		queryOddsParam.utcDateTime,
@@ -60,6 +59,13 @@ func getOddsUri(queryOddsParam queryOddsParam) string {
 		queryOddsParam.sortBy,
 		queryOddsParam.isFirstLoad,
 		queryOddsParam.MoreBetEvent)
+}
+
+type queryOddsParam struct {
+	utcDateTime                                                           int64
+	sportId, programmeId, pageType, displayView, pageNo, oddsType, sortBy int
+	uiBetType, MoreBetEvent                                               string
+	isFirstLoad                                                           bool
 }
 
 func getUnixUtcDateTimeNowMillisecond() int64 {
